ff870653ff141381fa03a3cd68bf00309cb18d99
WeiLin <nomorecomputer@gmail.com>
2026-01-14 13:25:28 +0800

    add all.scss , _style.css make sure work

--- src/App.jsx
+++ src/App.jsx
@@ -1,96 +1,11 @@
-import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+//import { useState } from "react";
+
 import { Modal } from "bootstrap";
-import axios from "axios";
+//import axios from "axios";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [productName, setProductName] = useState("");
-
-  const getObject = async () => {
-    const obj = await axios.get("https://api.restful-api.dev/objects/7");
-    setProductName(obj.data.name);
-  };
-
-  return (
-    <>
-      <button
-        type="button"
-        className="btn btn-primary"
-        data-bs-toggle="modal"
-        data-bs-target="#exampleModal"
-      >
-        Launch demo modal
-      </button>
-
-      <div
-        className="modal fade"
-        id="exampleModal"
-        tabIndex="-1"
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-      >
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h1 className="modal-title fs-5" id="exampleModalLabel">
-                Modal title
-              </h1>
-              <button
-                type="button"
-                className="btn-close"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-              ></button>
-            </div>
-            <div className="modal-body">...</div>
-            <div className="modal-footer">
-              <button
-                type="button"
-                className="btn btn-secondary"
-                data-bs-dismiss="modal"
-              >
-                Close
-              </button>
-              <button type="button" className="btn btn-primary">
-                Save changes
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <button className="btn btn-secondary" onClick={getObject}>
-        get object
-      </button>
-      <h2>{productName}</h2>
-      <div className="card">
-        <button
-          className="btn btn-primary"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          counter is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
